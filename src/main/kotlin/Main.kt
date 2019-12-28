3dae5b490ba8d20adc19d17a49c8ec026b9e361d
Casey Kulm <mlukyesac@gmail.com>
2019-12-28 15:23:16 -0700

    Cleanup main file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,9 +2,15 @@ fun main() {
     println("Spelling Bee booting up...")
 
     val dictionaryTool = DictionaryToolImpl()
+
+    // Map of Webster's Dictionary words to definitions. We mostly won't use the definitions, but they will be here
+    // if desired.
     val dictionary: Map<String, String> = dictionaryTool.getDictionary()
+
+    // Sequence of Webster's Dictionary words. This is the most simple dataset that we will modify.
     val wordSequence: Sequence<String> = dictionary.keys.asSequence()
 
+    // Sequence of words that are valid entry's for a Spelling Bee game.
     val beeWordSequence = wordSequence
         .applySpellingBeeRules(
             SpellingBeeFilterRule.AlphaOnly,
@@ -12,36 +18,16 @@ fun main() {
             SpellingBeeFilterRule.SevenOrFewerUnique
         )
 
-//    val uniqueCharacterToWordsMap = beeWordSequence.groupBy {
-//        UniqueCharacterSet(it)
-//    }
+    // Set of all possible Spelling Bee UniqueCharSets. This contains all possible Spelling Bee boards as well all
+    // subsets.
+    val beeUcsSet: Set<UniqueCharSet> = beeWordSequence.map { UniqueCharSet(it) }.toSet()
 
-    val ucsToWordMap = mutableMapOf<UniqueCharSet, MutableList<String>>()
+    // All possible Spelling Bee boards. A subset of the beeUcsSet, filtering for only UniqueCharSets which a unique
+    // count of 7 characters.
+    val beeBoards: Set<UniqueCharSet> = beeUcsSet.filter { it.uniqueCount == 7 }.toSet()
 
-    beeWordSequence.forEach { word ->
-        val ucs = UniqueCharSet(word)
-//        ucs.uniqueChars
-        var wordList: MutableList<String>? = ucsToWordMap[ucs]
+    beeBoards.forEach { println(it) }
 
-        if (wordList == null) {
-            wordList = mutableListOf()
-            ucsToWordMap[ucs] = wordList
-        }
-
-        wordList.add(word)
-    }
-
-    val beeUcsToWordMap = ucsToWordMap.filterKeys { it.uniqueCount == 7 }.toMutableMap()
-
-    beeUcsToWordMap.keys.forEach { beeUcs ->
-        // TODO: Form all subset UCS's possible,
-        //  get their words from ucsToWordMap,
-        //  add them to the lists for the beeUcs'
-    }
-
-//    ucsToWordMap.entries.take(100000).forEach { println("${it.key} : ${it.value}") }
-    beeUcsToWordMap.entries.take(1).forEach { println("${it.key.uniqueCharCombos} : ${it.value}") }
-
-    println("There's ${beeUcsToWordMap.size} possible sets")
+    println("There's ${beeBoards.size} possible boards")
 }
 
